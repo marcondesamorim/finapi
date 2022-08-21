80387eaddee874407bd6139c5a80fc5ff499ff5e
Marcondes Amorim <marcondesjra@gmail.com>
2022-08-20 22:27:53 -0300

    Correção do nome de gitignore

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
-const { application } = require('express');
 const express = require('express');
-
+const { v4: uuidv4 } = require('uuid');
 const app = express();
 
+const customers = [];
 app.post("/account", (request, response => {
     const { cpf, name } = request.body;
+    const id = uuidv4();
 
+    customers.push({
+        cpf,
+        name,
+        id,
+        statement: []
+    });
+    return response.send();
 }));
 
 app.listen(3333);
